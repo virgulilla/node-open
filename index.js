7e9f7ddd73dd59d71f6f5a86dea4483eaeb4b0f2
oscar <oscar.caasmontes@gmail.com>
2025-02-27 15:42:57 +0100

    implementacion de frontend

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ let persons = [
 ]
 
 app.use(cors())
+app.use(express.static('dist'))
 morgan.token('body', (request) => JSON.stringify(request.body))
 
 app.use(express.json())
@@ -36,10 +37,6 @@ app.use('/api/persons', morgan(':method :url :status :res[content-length] - :res
 }));
 
 
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World</h1>')
